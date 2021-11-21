1120b4cca2dfcd4fab7bc696e5a4bb2063a0afaf
Aidan Amato <aidanamato@comcast.net>
2021-11-21 11:31:10 -0500

    set up payment with stripe

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -7,9 +7,17 @@ import { idbPromise } from "../../utils/helpers";
 import CartItem from '../CartItem';
 import './style.css';
 
+import { useLazyQuery } from '@apollo/client';
+import { QUERY_CHECKOUT } from "../../utils/queries";
+
+import { loadStripe } from '@stripe/stripe-js';
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
+
 const Cart = () => {
   const [state, dispatch] = useStoreContext();
   console.log(state);
+
+  const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
   useEffect(() => {
     async function getCart() {
@@ -23,7 +31,15 @@ const Cart = () => {
     if (!state.cart.length) {
       getCart();
     }
-  }, [state.cart.length, dispatch])
+  }, [state.cart.length, dispatch]);
+
+  useEffect(() => {
+    if (data) {
+      stripePromise.then(res => {
+        res.redirectToCheckout({ sessionId: data.checkout.session });
+      });
+    }
+  }, [data]);
 
   function toggleCart() {
     dispatch({ type: TOGGLE_CART });
@@ -35,6 +51,20 @@ const Cart = () => {
       sum += item.price * item.purchaseQuantity;
     });
     return sum.toFixed(2);
+  }
+
+  function submitCheckout() {
+    const productIds = [];
+
+    state.cart.forEach(item => {
+      for (let i = 0; i < item.purchaseQuantity; i++) {
+        productIds.push(item._id);
+      }
+    });
+
+    getCheckout({
+      variables: { products: productIds }
+    });
   }
 
   if (!state.cartOpen) {
@@ -60,7 +90,7 @@ const Cart = () => {
         <strong>Total: ${calculateTotal()}</strong>
         {
           Auth.loggedIn() ?
-            <button>
+            <button onClick={submitCheckout}>
               Checkout
             </button>
             :
